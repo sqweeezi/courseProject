0e2971c14c3b1380ab7bfbe432e708607cc26738
Alexseev Oleg <olegaleks1997@gmail.com>
2019-11-26 20:55:17 +0700

    Добавлен DataSet

--- PropertyRegister/PropertyRegister/Program.cs
+++ PropertyRegister/PropertyRegister/Program.cs
@@ -16,7 +16,7 @@ namespace PropertyRegister
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
